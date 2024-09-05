8838f319107205cd8afe50389c6273571a7e5570
Antonio <antoniominafra.dev@gmail.com>
2024-09-05 16:05:32 +0200

    added a function delete in the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <section>
-        <header>
-            <h1>My Friends</h1>
-        </header>
-        <new-friend @add-contact="addContact"></new-friend>
-        <ul>
-          <friend-contact v-for="friend in friends"
-          :key="friend.id"
-          :id="friend.id"
-          :name="friend.name"
-          :phone-number="friend.phone"
-          :email-address="friend.email"
-          :is-favorite="friend.isFavorite"
-          @toggle-favorite="toggleFavoriteStatus"
-          ></friend-contact>
-        </ul>
-    </section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <new-friend @add-contact="addContact"></new-friend>
+    <ul>
+      <friend-contact v-for="friend in friends"
+      :key="friend.id"
+      :id="friend.id"
+      :name="friend.name"
+      :phone-number="friend.phone"
+      :email-address="friend.email"
+      :is-favorite="friend.isFavorite"
+      @toggle-favorite="toggleFavoriteStatus"
+      @delete="deleteContact"
+      ></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -39,7 +40,6 @@ export default {
         phone: "2335 294 46",
         email: "tony@gmail.com",
         isFavorite: false,
-
       },
       ]
     }
@@ -58,6 +58,9 @@ export default {
         isFavorite: false,
       };
       this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId){
+      this.friends = this.friends.filter( (friend) => friend.id !== friendId);
     }
   }
 }
@@ -75,71 +78,71 @@ export default {
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 html {
-    font-family: 'Jost', sans-serif;
+  font-family: 'Jost', sans-serif;
 }
 
 body {
-    margin: 0;
+  margin: 0;
 }
 
 header {
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    margin: 3rem auto;
-    border-radius: 10px;
-    padding: 1rem;
-    background-color: #58004d;
-    color: white;
-    text-align: center;
-    width: 90%;
-    max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
 
 #app ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 
 #app li, #app form {
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    margin: 1rem auto;
-    border-radius: 10px;
-    padding: 1rem;
-    text-align: center;
-    width: 90%;
-    max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
 
 #app h2 {
-    font-size: 2rem;
-    border-bottom: 4px solid #ccc;
-    color: #58004d;
-    margin: 0 0 1rem 0;
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
 }
 
 #app button {
-    font: inherit;
-    cursor: pointer;
-    border: 1px solid #ff0077;
-    background-color: #ff0077;
-    color: white;
-    padding: 0.05rem 1rem;
-    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
 
 #app button:hover,
 #app button:active {
-    background-color: #ec3169;
-    border-color: #ec3169;
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
 #app input {
-font: inherit;
-padding: 0.15rem;
+  font: inherit;
+  padding: 0.15rem;
 }
 
 #app label {
@@ -152,6 +155,5 @@ padding: 0.15rem;
 #app form div {
   margin: 1rem;
 }
-
 
 </style>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -7,6 +7,7 @@
             <li><strong>Phone:</strong>{{ phoneNumber }}</li>
             <li><strong>Email:</strong>{{ emailAddress }}</li>
         </ul>
+        <button @click="$emit('delete', id)">Delete</button>
     </li>
 </template>
 
@@ -36,7 +37,7 @@ export default {
             default: false,
         },
     },
-    emits: ['toggle-favorite'],
+    emits: ['toggle-favorite', 'delete'],
     data() {
         return {
             detailsAreVisible : false,
@@ -48,7 +49,11 @@ export default {
         },
         toggleFavorite(){
             this.$emit('toggle-favorite', this.id);
-        }
+        },
+        /* puoi fare la stessa cosa del toggleFavorite ma direttamente nel template come ho fatto con deleteFriend */
+        /* deleteFriend(){
+            this.$emit('delte', this.id);
+        } */
     },
 };
 </script>
