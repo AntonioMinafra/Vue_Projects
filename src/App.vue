43b9c79d2242c51421d683c9c0b42a54bc4db0f5
Antonio <antoniominafra.dev@gmail.com>
2024-09-05 09:34:32 +0200

    created new component FriendContact

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+        <header>
+            <h1>My Friends</h1>
+        </header>
+        <ul>
+          <friend-contact></friend-contact>
+          <friend-contact></friend-contact>
+
+        </ul>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      friends: [
+      {
+        id: "Manuel",
+        name: "Manuel Lorenz",
+        phone: "2345 234 56",
+        email: "manuel@gmail.com",
+      },
+      {
+        id: "tony",
+        name: "Tony Lorenz",
+        phone: "2335 294 46",
+        email: "tony@gmail.com",
+      },
+      ]
+    }
+  },
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
