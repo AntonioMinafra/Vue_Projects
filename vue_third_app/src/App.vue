ec06564aaa13ece9c28c56f01a017e2616da54fd
Antonio <antoniominafra.dev@gmail.com>
2024-09-06 11:33:05 +0200

    added scoped in the style and change the components in local

--- vue_third_app/src/App.vue
+++ vue_third_app/src/App.vue
@@ -11,7 +11,16 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    'the-header': TheHeader,
+    'badge-list': BadgeList,
+    'user-info': UserInfo,
+  },
   data() {
     return {
       activeUser: {
@@ -24,7 +33,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 html {
   font-family: sans-serif;
 }
