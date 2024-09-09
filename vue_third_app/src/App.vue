7ab2e5470a817169971c3cdc9ea85b1b28ed0ebd
Antonio <antoniominafra.dev@gmail.com>
2024-09-09 11:09:11 +0200

    created CourseGoals components

--- vue_third_app/src/App.vue
+++ vue_third_app/src/App.vue
@@ -1,25 +1,32 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <div>
+      <the-header></the-header>
+      <badge-list></badge-list>
+      <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
-  </div>
+      ></user-info>
+      <course-goals #default="slotProps">
+        <h2> {{slotProps.item}}</h2>
+        <!-- lasciato in sospeso, non visualizzo nel frontend il secondo prop -->
+        <p> {{slotProps["another-prop"]}}</p>
+      </course-goals>
+    </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     'the-header': TheHeader,
     'badge-list': BadgeList,
     'user-info': UserInfo,
+    'course-goals': CourseGoals,
   },
   data() {
     return {
