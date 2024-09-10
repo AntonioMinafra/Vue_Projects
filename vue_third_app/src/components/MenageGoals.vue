6b47960600f3ebc1366aecb003e0fa4758bc346d
Antonio <antoniominafra.dev@gmail.com>
2024-09-10 10:35:19 +0200

    update the syntax

--- vue_third_app/src/components/MenageGoals.vue
+++ vue_third_app/src/components/MenageGoals.vue
@@ -1,40 +1,40 @@
 <template>
-    <div>
-        <h2>Menage Goals</h2>
-        <input type="text" ref="goal">
-        <button @click="setGoals">Set Goal</button>
-        <Teleport to="body">
-            <ErrorAlert v-if="inputIsInvalid">
-                <h2>Input is Invalid!</h2>
-                <p>Please enter at least a few characters...</p>
-                <button @click="confirmedButton">Okay!</button>
-            </ErrorAlert>
-        </Teleport>
-    </div>
+  <div>
+    <h2>Menage Goals</h2>
+    <input type="text" ref="goal">
+    <button @click="setGoals">Set Goal</button>
+    <Teleport to="body">
+      <ErrorAlert v-if="inputIsInvalid">
+        <h2>Input is Invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmedButton">Okay!</button>
+      </ErrorAlert>
+    </Teleport>
+  </div>
 </template>
 
 <script>
 import ErrorAlert from './ErrorAlert.vue';
 
- export default {
-    components: {
-        ErrorAlert
+export default {
+  components: {
+    ErrorAlert
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    }
+  },
+  methods: {
+    setGoals() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
     },
-    data() {
-        return {
-            inputIsInvalid: false,
-        }
-    },
-    methods: {
-        setGoals() {
-            const enteredValue = this.$refs.goal.value;
-            if (enteredValue === '') {
-                this.inputIsInvalid = true;
-            }
-        },
-        confirmedButton(){
-            this.inputIsInvalid = false;
-        }
-    },
- }
+    confirmedButton(){
+      this.inputIsInvalid = false;
+    }
+  },
+}
 </script>
