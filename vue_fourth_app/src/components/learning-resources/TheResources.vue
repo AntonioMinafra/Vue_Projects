f103798c6f5b510902860886aa2ee7081773c82f
Antonio <antoniominafra.dev@gmail.com>
2024-09-11 16:15:02 +0200

    fixed the component TheResources

--- vue_fourth_app/src/components/learning-resources/TheResources.vue
+++ vue_fourth_app/src/components/learning-resources/TheResources.vue
@@ -1,7 +1,7 @@
 <template>
     <base-card>
-    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-    <base-button @click="setSelectedTab('add-resources')">Add Resource</base-button>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storeResButton">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resources')" :mode="addResButton">Add Resource</base-button>
     </base-card>
     <component :is="selectedTab"></component>
 </template>
@@ -17,8 +17,25 @@ export default {
   components: { BaseCard, BaseButton, StoredResources, AddResources },
   data() {
     return {
-        selectedTab: 'stored-resources'
+        selectedTab: 'stored-resources',
+        storedResources: [
+        { id: 'official-guide', title: 'Official Guide', description: 'The official Vue.js documentation', link: 'https://vuejs.org' },
+        { id: 'google', title: 'Google', description: 'Learn to google...', link: 'https://google.com' }
+      ]
     }
+  },
+  computed: {
+    storeResButton(){
+        return this.selectedTab === 'stored-resources' ? null : 'flat'
+    },
+    addResButton(){
+        return this.selectedTab === 'add-resources' ? null : 'flat'
+    }
+  },
+  provide() {
+    return {
+        resources: this.storedResources
+    };
   },
   methods: {
     setSelectedTab(tab){
