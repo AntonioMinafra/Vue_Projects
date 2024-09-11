0a1097df0dfa90a916cba67f4d1b26f6215f41cd
Antonio <antoniominafra.dev@gmail.com>
2024-09-11 17:14:55 +0200

    added a modal dialog

--- vue_fourth_app/src/components/learning-resources/TheResources.vue
+++ vue_fourth_app/src/components/learning-resources/TheResources.vue
@@ -1,9 +1,11 @@
 <template>
-    <base-card>
-    <base-button @click="setSelectedTab('stored-resources')" :mode="storeResButton">Stored Resources</base-button>
-    <base-button @click="setSelectedTab('add-resources')" :mode="addResButton">Add Resource</base-button>
-    </base-card>
-    <component :is="selectedTab"></component>
+  <base-card>
+  <base-button @click="setSelectedTab('stored-resources')" :mode="storeResButton">Stored Resources</base-button>
+  <base-button @click="setSelectedTab('add-resources')" :mode="addResButton">Add Resource</base-button>
+</base-card>
+<keep-alive>
+  <component :is="selectedTab"></component>
+</keep-alive>
 </template>
 
 <script>
@@ -17,29 +19,40 @@ export default {
   components: { BaseCard, BaseButton, StoredResources, AddResources },
   data() {
     return {
-        selectedTab: 'stored-resources',
-        storedResources: [
-        { id: 'official-guide', title: 'Official Guide', description: 'The official Vue.js documentation', link: 'https://vuejs.org' },
-        { id: 'google', title: 'Google', description: 'Learn to google...', link: 'https://google.com' }
+      selectedTab: 'stored-resources',
+      storedResources: [
+      { id: 'official-guide', title: 'Official Guide', description: 'The official Vue.js documentation', link: 'https://vuejs.org' },
+      { id: 'google', title: 'Google', description: 'Learn to google...', link: 'https://google.com' }
       ]
     }
   },
   computed: {
     storeResButton(){
-        return this.selectedTab === 'stored-resources' ? null : 'flat'
+      return this.selectedTab === 'stored-resources' ? null : 'flat'
     },
     addResButton(){
-        return this.selectedTab === 'add-resources' ? null : 'flat'
+      return this.selectedTab === 'add-resources' ? null : 'flat'
     }
   },
   provide() {
     return {
-        resources: this.storedResources
+      resources: this.storedResources,
+      addResource: this.addResource
     };
   },
   methods: {
     setSelectedTab(tab){
-        this.selectedTab = tab;
+      this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newResources = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: link
+      };
+      this.storedResources.unshift(newResources);
+      this.selectedTab = 'stored-resources';
     }
   },
 
