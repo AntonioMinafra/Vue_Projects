d672c294fa70558114d196e68613514afdf9e8f1
Antonio <antoniominafra.dev@gmail.com>
2024-09-11 17:46:23 +0200

    added a delete function

--- vue_fourth_app/src/components/learning-resources/LearningResource.vue
+++ vue_fourth_app/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3> {{ title }}</h3>
-        <base-button mode="flat">Delete</base-button>
+        <base-button mode="flat" @click="deleteResource(id)">Delete</base-button>
       </header>
       <p> {{ description }}</p>
       <nav>
@@ -19,7 +19,8 @@ import BaseCard from '../UI/BaseCard.vue'
 
 export default {
   components: { BaseCard, BaseButton },
-  props: ['title', 'description', 'link']
+  props: ['id', 'title', 'description', 'link'],
+  inject: ['deleteResource']
 
 }
 
