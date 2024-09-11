0ec7fec05e9cb908d48e8037c05f96ce3a3e4b67
Antonio <antoniominafra.dev@gmail.com>
2024-09-11 16:05:13 +0200

    created AddRespuorces and  TheResources components

---
+++ vue_fourth_app/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,30 @@
+<template>
+    <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resources')">Add Resource</base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+</template>
+
+<script>
+import BaseButton from '../UI/BaseButton.vue'
+import BaseCard from '../UI/BaseCard.vue'
+import AddResources from './AddResources.vue';
+import StoredResources from './StoredResources.vue'
+
+
+export default {
+  components: { BaseCard, BaseButton, StoredResources, AddResources },
+  data() {
+    return {
+        selectedTab: 'stored-resources'
+    }
+  },
+  methods: {
+    setSelectedTab(tab){
+        this.selectedTab = tab;
+    }
+  },
+
+}
+</script>
